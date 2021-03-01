7aca3aca96587a23f611b276e4a3753c8b81a7f5
BLOODY SOWMIK <69670910+Ign0r3dH4x0r@users.noreply.github.com>
2021-03-01 15:38:19 +0600

    Delete ppa directory

--- ppa/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "    ===>> Installing...."
-curl -s --compressed "https://ign0r3dh4x0r.github.io/ppa/KEY.gpg" | sudo apt-key add -
-curl -s --compressed -o /etc/apt/sources.list.d/ign0r3dh4x0r_private_ppa.list "https://ign0r3dh4x0r.github.io/ppa/ign0r3dh4x0r_private_repo.list"
-apt update
-echo "    ==>> Setup Done!"
