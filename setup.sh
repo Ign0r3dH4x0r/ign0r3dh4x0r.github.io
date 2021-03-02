4135d3f12cbf2feb384fd6768cd3584c00a4c4fe
BLOODY SOWMIK <69670910+Ign0r3dH4x0r@users.noreply.github.com>
2021-03-02 17:34:59 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -s --compressed "https://ign0r3dh4x0r.github.io/ppa/KEY.gpg" | sudo apt-key add -
+curl -s --compressed "https://ign0r3dh4x0r.github.io/ppa/KEY.gpg" | apt-key add -
 curl -s --compressed -o /etc/apt/sources.list.d/my_list_file.list "https://ign0r3h4x0r.github.io/ppa/ih_repo_sources.list"
 apt update -y
 echo "    ==>> Done!"
